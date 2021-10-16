d2d5a71abe30f93876486af8ada90212c9470383
irsanmardiansyah <irsanmardiansyah97@gmail.com>
2021-10-16 22:49:58 +0700

    remove unused code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,4 @@
-import profile.UserController;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.print("Hell World ");
     }
